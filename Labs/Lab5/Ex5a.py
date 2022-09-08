b96ebc465e4d685c94f17c1a46a261a744410ebc
Jaysenso <jsenongxuan@gmail.com>
2022-09-08 14:48:56 +0800

    SC1013

--- Labs/Lab5/Ex5a.py
+++ Labs/Lab5/Ex5a.py
@@ -1,18 +1,37 @@
 from sense_hat import SenseHat
-from time import sleep
-import random
-
 sense = SenseHat()
+sense.set_rotation(0)
+sense.clear()
 
-while True:
-    sense.clear()
-    for positions in range(4):
-        xIndex = random.randint(0,7)
-        yIndex = random.randint(0,7)
-        randomcolor = (random.randint(0,255),random.randint(0,255),random.randint(0,255))
-        print(randomcolor)
-        sense.set_pixel(xIndex,yIndex,tuple(randomcolor))
-    sleep(1)
-        
+#--- function get_color() ---------------------------
+def get_color(color):
+    keep_looping = True
+    color_str = 0
+    no_of_try , tries_left = 1 , 4
 
-    
+    while keep_looping: 
+        tries_left = 3 - no_of_try #tries_left keep track of the number of tries with respect to no_of_try in the exception handling
+        if tries_left < 4:
+            try: #request an int input - if input != int, loop will be thrown into exception handling below
+                color_str=int(input("Enter the value of the {} color for message (0 to 255):".format(color)))
+                if color_str in range(0,256):
+                    return color_str
+                else: #call user out if input not within range
+                    print("Input not in range between 0-255 [No of tries left:{}]".format(tries_left))
+                    no_of_try +=1
+            except:#exception handling if input != int
+                no_of_try +=1
+                print("False input - Integers only [No of tries left:{}]".format(tries_left))
+                
+        else:
+            print("You have enter a invalid value after 3 tries!")
+            return 0
+
+     
+#main body program
+r_int = get_color("red")
+g_int = get_color("green")
+b_int = get_color("blue")
+print(r_int,g_int,b_int)
+msg_color = (r_int, g_int, b_int)
+sense.show_message("I got it!", text_colour=msg_color)
